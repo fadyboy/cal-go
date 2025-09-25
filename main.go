76e31647491cd3396d01902c10fdbfc9884b3651
Anthony Fadero <aofadero.com>
2025-09-24 22:47:36 -0400

    Refactore main

--- main.go
+++ main.go
@@ -4,20 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/fadyboy/lenslocked/controllers"
-	"github.com/fadyboy/lenslocked/models"
-	"github.com/fadyboy/lenslocked/templates"
-	"github.com/fadyboy/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/csrf"
+
+	"github.com/fadyboy/lenslocked/controllers"
+	"github.com/fadyboy/lenslocked/migrations"
+	"github.com/fadyboy/lenslocked/models"
+	"github.com/fadyboy/lenslocked/templates"
+	"github.com/fadyboy/lenslocked/views"
 )
 
 func main() {
-	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)
-
 	// setup db connection
 	cfg := models.DefaultDBConfig()
 	db, err := models.Open(cfg)
@@ -26,6 +24,12 @@ func main() {
 	}
 	defer db.Close()
 
+	err = models.MigrateFS(db, migrations.FS, ".")
+	if err != nil {
+		panic(err)
+	}
+
+	// Set up services
 	userService := models.UserService{
 		DB: db,
 	}
@@ -34,61 +38,58 @@ func main() {
 		DB: db,
 	}
 
-	usersC := controllers.Users{
-		UserService:    &userService,
-		SessionService: &sessionService,
-	}
-
-	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))
-
-	r.Get("/", controllers.StaticHandler(tpl))
-
-	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))
-
-	r.Get("/contact", controllers.StaticHandler(tpl))
-
-	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))
-
-	r.Get("/faq", controllers.FAQ(tpl))
-
-	tpl = views.Must(views.ParseFS(templates.FS, "about.gohtml", "tailwind.gohtml"))
-
-	r.Get("/about", controllers.StaticHandler(tpl))
-
-	// Signup
-	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
-	r.Get("/signup", usersC.New)
-
-	r.Post("/signup", usersC.Create)
-
-	// SignIn
-	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
-	r.Get("/signin", usersC.SignIn)
-	r.Post("/signin", usersC.ProcessSignIn)
-
-	// signed in user
-	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "current_user.gohtml", "tailwind.gohtml"))
-	r.Get("/users/me", usersC.CurrentUser)
-
-	// Signout
-	r.Post("/signout", usersC.ProcessSignOut)
-
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Page Not Found", http.StatusNotFound)
-	})
-
 	// middleware for setting user context
 	umw := controllers.UserMiddleWare{
 		SessionService: &sessionService,
 	}
 
+	// Set up controllers
+	usersC := controllers.Users{
+		UserService:    &userService,
+		SessionService: &sessionService,
+	}
+
+	usersC.Templates.New = views.Must(views.ParseFS(
+		templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(
+		templates.FS, "signin.gohtml", "tailwind.gohtml"))
+
 	// csrf
-	csrfKey := "32-byte-long-auth-key"
+	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
 	csrfMw := csrf.Protect(
 		[]byte(csrfKey),
 		csrf.Secure(false),
-		csrf.TrustedOrigins([]string{"localhost:3000"}),
+		csrf.TrustedOrigins([]string{"localhost:3000"}), // TODO: Fix before deploying
 	)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"home.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"contact.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/about", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"about.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
+		templates.FS,
+		"faq.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/signup", usersC.New)
+	r.Post("/signup", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
+	r.Post("/signout", usersC.ProcessSignOut)
+	r.Get("/users/me", usersC.CurrentUser)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	})
 
 	fmt.Println("Starting the server on port :3000")
 	http.ListenAndServe(":3000", csrfMw(umw.SetUser(r)))
--- models/postgres.go
+++ models/postgres.go
@@ -3,8 +3,10 @@ package models
 import (
 	"database/sql"
 	"fmt"
+	"io/fs"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/pressly/goose/v3"
 )
 
 type DBConfig struct {
@@ -23,12 +25,12 @@ func (cfg DBConfig) String() string {
 
 func DefaultDBConfig() DBConfig {
 	return DBConfig{
-		Host: "localhost",
-		Port: "5437",
-		User: "lenslock",
+		Host:     "localhost",
+		Port:     "5437",
+		User:     "lenslock",
 		Password: "password",
 		Database: "lenslocked",
-		SSLMode: "disable",
+		SSLMode:  "disable",
 	}
 }
 
@@ -39,4 +41,30 @@ func Open(config DBConfig) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+func Migrate(db *sql.DB, dir string) error {
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("migrate dialect error: %w", &err)
+	}
+
+	err = goose.Up(db, dir)
+	if err != nil {
+		return fmt.Errorf("migrate up error: %w", &err)
+	}
+
+	return nil
+}
+
+func MigrateFS(db *sql.DB, migrationFS fs.FS, dir string) error {
+	if dir == "" {
+		dir = "."
+	}
+
+	goose.SetBaseFS(migrationFS)
+	defer func() {
+		goose.SetBaseFS(nil)
+	}()
+	return Migrate(db, dir)
 }
